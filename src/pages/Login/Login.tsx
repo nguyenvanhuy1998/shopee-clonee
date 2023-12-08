53fd05188fcaf21919e9ef6344e282bde5194315
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2023-12-08 14:21:05 +0700

    Code UI trang Login và Register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,45 @@
+import { Link } from 'react-router-dom'
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='bg-orange'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 bg-white rounded shadow-sm'>
+              <div className='text-2xl'>Đăng nhập</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                />
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                />
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full text-center bg-red-500 py-4 px-2 text-white text-sm hover:bg-red-600'>
+                  Đăng nhập
+                </button>
+              </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+                <Link to={'/register'} className='text-red-400 ml-1'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
