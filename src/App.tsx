bf58afc6da83909488c5d43b1e79c2995af4e6e2
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2023-12-14 16:02:15 +0700

    Xử lý lỗi message đơn thuần với interceptor Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ToastContainer } from 'react-toastify'
 import useRouteElements from './useRouteElements'
-
+import 'react-toastify/dist/ReactToastify.css'
 export default function App() {
   const routeElements = useRouteElements()
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
